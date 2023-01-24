3c40c8352ae489fa4a057c2208a3663ae5c5aa56
unknown <gownideepu23@gmail.com>
2023-01-24 16:06:46 +0530

    add

--- src/Com/Bridgelabs/Contact.java
+++ src/Com/Bridgelabs/Contact.java
@@ -1,5 +1,111 @@
 package Com.Bridgelabs;
 
+
 public class Contact {
+	//setting the variables for the adress book
+	private String firstName;
+	private String lastName;
+	private String address;
+	private int zip;
+	private String phoneNumber;
+	private String email;
+	
+	public Contact() {
+    }
+
+	//method to get the detailes 
+    public Contact(String firstName, String lastName,String address,int zip, String email, String phoneNumber) {
+    	//arching the encptulation
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address=address;
+        this.zip=zip;    
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public int getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
